7ecc61b97b6b0015e3dfdf387e143ce0d9fbd54a
g <gennadas_1@proton.me>
2026-06-25 17:58:03 +0300

    +

---
+++ .wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+return {
+  default_prog = { 'wsl.exe', '-d', 'archlinux', '--', '/usr/bin/tmux' },
+  font = wezterm.font("JetBrains Mono"),
+  font_size = 12.0,
+  window_decorations = "RESIZE",
+  keys = {
+    { key = 'C', mods = 'CTRL|SHIFT', action = act.CopyTo 'Clipboard' },
+    { key = 'V', mods = 'CTRL|SHIFT', action = act.PasteFrom 'Clipboard' },
+  },
+}
